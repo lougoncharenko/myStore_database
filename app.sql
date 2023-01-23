e3b772161db7482a61cf3ebda7454ddceeea8e49
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2023-01-23 14:25:14 -0500

    added case, new tables and inserted into tables

--- app.sql
+++ app.sql
@@ -32,3 +32,75 @@ SHOW TABLES;
 -- describe tables
 DESCRIBE items;
 DESCRIBE customers;
+
+-- Creating another table
+CREATE TABLE orders (
+order_id int AUTO_INCREMENT PRIMARY KEY,
+customer_id int NOT NULL,
+order_date date NOT NULL,
+total decimal(7,2),
+FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+-- inserting values
+INSERT INTO orders(customer_id, order_date, total)
+VALUES
+(4, "2020-01-04", 8894.95),
+(3, "2020-02-13", 100.34),
+(3, "2019-03-20", 3045.65),
+(1, "2019-08-01", 200.56),
+(2, "2019-09-09", 1078.99);
+
+CREATE TABLE companies ( 
+company_id int AUTO_INCREMENT PRIMARY KEY, 
+company_name varchar(100)
+);
+
+INSERT INTO companies (company_name)
+VALUES
+("Philips"),
+("Sega"),
+("Atari"),
+("Sony"),
+("Nintendo"),
+("Sinclair");
+
+-- Altering table
+ALTER table items
+ADD COLUMN company_id int,
+ADD FOREIGN KEY (company_id) REFERENCES companies(company_id);
+
+
+-- Using Case to update
+UPDATE items
+SET name = CASE item_id
+WHEN 1 THEN "Master System"
+WHEN 2 THEN "Lynx"
+WHEN 3 THEN "CD-I"
+WHEN 4 THEN "Virtual Boy"
+WHEN 6 THEN "Jaguar"
+WHEN 7 THEN "64"
+WHEN 8 THEN "Genesis"
+WHEN 9 THEN "Mega Drive II"
+WHEN 10 THEN "Playstation"
+WHEN 11 THEN "Game Boy"
+WHEN 12 THEN "ZX Spectrum"
+END;
+
+SELECT * FROM items;
+
+UPDATE items
+SET company_id = CASE item_id
+WHEN 1 THEN 2
+WHEN 2 THEN 3
+WHEN 3 THEN 1
+WHEN 4 THEN 5
+WHEN 6 THEN 3
+WHEN 7 THEN 5
+WHEN 8 THEN 2
+WHEN 9 THEN 2
+WHEN 10 THEN 4
+WHEN 11 THEN 5
+WHEN 12 THEN 6
+END;
+
+SELECT * FROM items;
